e07d8e5aa064c0ce35cdd09fcd50b4fee3cc6966
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-06-17 13:05:44 +0100

    Create Files.cpp

---
+++ Files.cpp
@@ -0,0 +1,6 @@
+getgenv().DesireReborn = true
+
+-- hookfunc gotten from this thread lmao: https://wearedevs.net/forum/t/21263/
+getgenv().hookfunction = function(func, func_)
+    func = func_
+end
