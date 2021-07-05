cff87261ce7cdf923ebd25d1549172ff49f60788
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2021-07-05 10:41:24 +0100

    Update Files.cpp

--- Files.cpp
+++ Files.cpp
@@ -1,37 +1,10 @@
 getgenv().DesireReborn = true
 
--- hookfunc gotten from this thread lmao: https://wearedevs.net/forum/t/21263/
+--[[ hookfunc gotten from this thread lmao: https://wearedevs.net/forum/t/21263/
 getgenv().hookfunction = function(func, func_)
     func = func_
 end
-
-getgenv().hookfunctions = function(func, func_)
-    func = func_
-end
-
-getgenv().hookfunc = function(func, func_)
-    func = func_
-end
-
-getgenv().hookfuncs = function(func, func_)
-    func = func_
-end
-
-getgenv().connectfunction = function(func, func_)
-    func = func_
-end
-
-getgenv().connectfunctions = function(func, func_)
-    func = func_
-end
-
-getgenv().connectfuncs = function(func, func_)
-    func = func_
-end
-
-getgenv().connectfunc = function(func, func_)
-    func = func_
-end
+--]]
 
 getgenv().loadtext = function(url)
     loadstring(game:HttpGet(tostring(url)))()
